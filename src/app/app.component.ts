6a2f75fb86753c2fc0c863b505fe142d5a6d3b08
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2023-04-05 13:10:07 +0200

    Parameters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.moviesInTheaters = [
+        {
+          title: 'Spider-Man',
+          releaseDate: new Date(),
+          price: 1400.99,
+        },
+        {
+          title: 'Moana',
+          releaseDate: new Date('2016-11-14'),
+          price: 300.99,
+        },
+    ];
+    
+    this.moviesFutureReleases = [
+      {
+        title: 'Avengers',
+        releaseDate: new Date(),
+        price: 456.99,
+      },
+      {
+        title: 'Toy Story',
+        releaseDate: new Date('2020-11-14'),
+        price: 789.99,
+      },
+    ];
+  }
+
   title = 'angular-movies';
+  moviesInTheaters!: { title: string; releaseDate: Date; price: number }[];
+  moviesFutureReleases!: { title: string; releaseDate: Date; price: number }[];
 
   duplicateNumber(n: number) {
     return n * 2;
--- src/app/movies/movies-list/movies-list.component.ts
+++ src/app/movies/movies-list/movies-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-movies-list',
@@ -8,24 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class MoviesListComponent implements OnInit {
   constructor() {}
 
-  movies!: { title: string; releaseDate: Date; price: number }[];
-  timeout = 2000;
-
   ngOnInit(): void {
-    setTimeout(() => {
-      // this.movies = [];
-      this.movies = [
-        {
-          title: 'Spider-Man',
-          releaseDate: new Date(),
-          price: 1400.99,
-        },
-        {
-          title: 'Moana',
-          releaseDate: new Date('2016-11-14'),
-          price: 300.99,
-        },
-      ];
-    }, this.timeout);
   }
+
+  @Input()
+  movies!: { title: string; releaseDate: Date; price: number }[];
 }
