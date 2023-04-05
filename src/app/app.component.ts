a46e967acee5590feb19ba19c3f2c90733bb87d9
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2023-04-05 11:31:33 +0200

    NgIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  movies!: { title: string; releaseDate: Date; price: number }[];
+  timeout = 0;
   title = 'angular-movies';
 
-  movies = [
-    {
-      title: 'Spider-Man',
-      releaseDate: new Date(),
-      price: 1400.99,
-    },
-    {
-      title: 'Moana',
-      releaseDate: new Date('2016-11-14'),
-      price: 300.99,
-    },
-  ];
+  ngOnInit(): void {
+    setTimeout(() => {
+      // this.movies = [];
+      this.movies = [
+        {
+          title: 'Spider-Man',
+          releaseDate: new Date(),
+          price: 1400.99,
+        },
+        {
+          title: 'Moana',
+          releaseDate: new Date('2016-11-14'),
+          price: 300.99,
+        },
+      ];
+    }, this.timeout);
+  }
 
   duplicateNumber(n: number) {
     return n * 2;
