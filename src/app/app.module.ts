a166aba7138fe79f21c0919a971ba23e986edb5b
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2023-04-06 10:56:39 +0200

    ngClass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { GenericListComponent } from './utilities/generic-list/generic-list.comp
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { MenuComponent } from './menu/menu.component';
+import { RatingComponent } from './utilities/rating/rating.component';
 @NgModule({
   declarations: [
     AppComponent,
     MoviesListComponent,
     GenericListComponent,
-    MenuComponent
+    MenuComponent,
+    RatingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/utilities/rating/rating.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rating',
+  templateUrl: './rating.component.html',
+  styleUrls: ['./rating.component.css'],
+})
+export class RatingComponent implements OnInit {
+  ngOnInit(): void {
+    this.maxRatingArray = Array(this.maxRating).fill(0);
+  }
+
+  @Input()
+  maxRating = 5;
+  @Input()
+  selectedRate = 0;
+  previousRate = 0;
+  maxRatingArray?: {}[];
+
+  handleMouseEnter(index: number) {
+    this.selectedRate = index + 1;
+  }
+
+  handleMouseLeave() {
+    if (this.previousRate !== 0) {
+      this.selectedRate = this.previousRate;
+    } else {
+      this.selectedRate = 0;
+    }
+  }
+
+  rate(index: number) {
+    this.selectedRate = index + 1;
+    this.previousRate = this.selectedRate;
+  }
+}
