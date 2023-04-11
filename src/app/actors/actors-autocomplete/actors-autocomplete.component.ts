af6cdd188867e979975dbfb35cef5c3d38146246
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2023-04-11 19:30:36 +0200

    Mat-table component

--- src/app/actors/actors-autocomplete/actors-autocomplete.component.ts
+++ src/app/actors/actors-autocomplete/actors-autocomplete.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'app-actors-autocomplete',
@@ -37,7 +38,7 @@ export class ActorsAutocompleteComponent implements OnInit {
 
   columnsToDisplay = ['picture', 'name', 'character', 'actions'];
 
-  // @ViewChild(MatTable) table: MatTable<any>;
+  @ViewChild(MatTable) table?: MatTable<any>;
 
   ngOnInit(): void {
     this.control.valueChanges.subscribe((value: string) => {
@@ -54,16 +55,16 @@ export class ActorsAutocompleteComponent implements OnInit {
     console.log(event.option.value);
     this.selectedActors.push(event.option.value);
     this.control.patchValue('');
-    // if (this.table !== undefined) {
-    //   this.table.renderRows();
+    if (this.table !== undefined) {
+      this.table.renderRows();
+    }
   }
-  // }
 
-  // remove(actor) {
-  //   const index = this.selectedActors.findIndex((a) => a.name === actor.name);
-  //   this.selectedActors.splice(index, 1);
-  //   this.table.renderRows();
-  // }
+  remove(actor: any) {
+    const index = this.selectedActors.findIndex((a) => a.name === actor.name);
+    this.selectedActors.splice(index, 1);
+    this.table?.renderRows();
+  }
 
   // dropped(event: CdkDragDrop<any[]>) {
   //   const previousIndex = this.selectedActors.findIndex(
