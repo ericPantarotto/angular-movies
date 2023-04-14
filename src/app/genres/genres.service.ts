944f06316f788e19f307c8659864ca26e0711596
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2023-04-14 23:34:49 +0200

    Creating a service

---
+++ src/app/genres/genres.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { genreDTO } from './genres.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GenresService {
+
+  constructor() { }
+
+  getAll(): genreDTO[] {
+    return [{
+      id: 1, name: 'Drama'}]
+  }
+}
--- src/app/genres/index-genres/index-genres.component.ts
+++ src/app/genres/index-genres/index-genres.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GenresService } from '../genres.service';
 
 @Component({
   selector: 'app-index-genres',
   templateUrl: './index-genres.component.html',
   styleUrls: ['./index-genres.component.css']
 })
-export class IndexGenresComponent {
-
-}
+export class IndexGenresComponent implements OnInit {
+  constructor(private genreService: GenresService) { }
+  
+  ngOnInit(): void {
+    const genres = this.genreService.getAll();
+    console.log(genres)
+  }
+};
