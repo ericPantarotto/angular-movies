d750b6aff52289ed78fd506d3939f6c175ad1cfd
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2023-04-18 18:58:23 +0200

    index movie theaters

--- src/app/movie-theaters/index-theaters/index-theaters.component.ts
+++ src/app/movie-theaters/index-theaters/index-theaters.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieTheatersService } from '../movie-theaters.service';
+import { movieTheatersDTO } from '../movie-theaters.model';
 
 @Component({
   selector: 'app-index-theaters',
   templateUrl: './index-theaters.component.html',
-  styleUrls: ['./index-theaters.component.css']
+  styleUrls: ['./index-theaters.component.css'],
 })
-export class IndexTheatersComponent {
+export class IndexTheatersComponent implements OnInit {
+  movieTheaters: movieTheatersDTO[] = [];
+  displayColumns = ['name', 'actions'];
+  
+  constructor(private movieTheatersService: MovieTheatersService) {}
 
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData() {
+    this.movieTheatersService
+      .get()
+      .subscribe((movieTheaters) => (this.movieTheaters = movieTheaters));
+  }
+
+  delete(id: number) {
+    this.movieTheatersService.delete(id).subscribe(() => this.loadData());
+  }
 }
